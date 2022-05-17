067f235763391915597520c6954613d462054cda
Abe Massry <a@abemassry.com>
2022-05-16 23:31:49 -0400

    add create store placeholders

--- cmd/store.go
+++ cmd/store.go
@@ -10,6 +10,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// StoreName is the name of the key value store
+var StoreName string
+
 // Key is the key in the key value store
 var Key string
 
@@ -28,7 +31,13 @@ var storeCmd = &cobra.Command{
 A key is a string and value can be anything but defaults to a string and is
 stored as a URL
 
-wkv store --key="foo" --value="bar" --type="string"
+wkv store --store-name="foo" --key="bar" --value="baz" --type="string"
+    --store-name name of the key value store container
+    --key name of the key inside key value store
+    --value either a string (default) or a file based on the --type
+      in either case a file is uploaded which allows the string data to be
+      incredbily long and is referenced by a URL pointing to the file.
+    --type string or file
 value is either a string (default) or a file specified by --type="file"
 if a file is specified the path is either absolute or the default is the
 current directory`,
@@ -40,6 +49,9 @@ current directory`,
 		fmt.Println("")
 		fmt.Println("value:")
 		fmt.Println(Value)
+		fmt.Println("")
+		fmt.Println("store-name:")
+		fmt.Println(StoreName)
 	},
 }
 
@@ -55,9 +67,11 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// storeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	storeCmd.Flags().StringVarP(&StoreName, "store-name", "s", "", "name of the key value store")
 	storeCmd.Flags().StringVarP(&Key, "key", "k", "", "name of the key")
 	storeCmd.Flags().StringVarP(&Value, "value", "v", "", "either text or the name of a file")
 	storeCmd.Flags().StringVarP(&Type, "type", "t", "string", "the type of value, defaults to string")
+	storeCmd.MarkFlagRequired("store-name")
 	storeCmd.MarkFlagRequired("key")
 	storeCmd.MarkFlagRequired("value")
 }
