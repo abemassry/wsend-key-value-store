9f5b8ba60cf1be73eef002d41bd00eed48c940f5
Abe Massry <a@abemassry.com>
2022-06-01 22:57:11 -0400

    add start of store command

--- cmd/get.go
+++ cmd/get.go
@@ -13,15 +13,35 @@ import (
 // getCmd represents the get command
 var getCmd = &cobra.Command{
 	Use:   "get",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "wsend-key-value store get a value based on a key",
+	Long: `wsend-key-value store get a value based on a key
+This command gets a value given a key and a store
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+wkv get --store-link="https://wsnd.io/IdGzDoh/foo" --key="bar"
+    --store-link name of the key value store container
+    --key name of the key inside key value store
+    --action optional
+      "print" prints the text content if this is a link, it prints the link
+      "download" downloads the file to the current directory
+      "dump" the default, dumps the contents of the linked file if the link is
+      a file
+    --uid is optionally passed in like in create`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("get called")
+		if UID == "" {
+			UID = getUID()
+		}
+		if UID == "" {
+			fmt.Println(fmt.Errorf("Could not find uid"))
+			os.Exit(1)
+		}
+		UID = strings.TrimSpace(UID)
+		contents := []byte(Value)
+		valueLink := ""
+
+
+		extraParams := map[string]string{
+			"uid": UID,
+		}
 	},
 }
 
